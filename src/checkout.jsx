3d111e800ea8366adbe033448690fec73893e1e3
Robin.Ong <robin.ong@bigcommerce.com>
2018-03-01 09:43:49 +1100

    Import Checkout SDK

--- src/checkout.jsx
+++ src/checkout.jsx
@@ -1,6 +1,25 @@
 import React from 'react';
+import { createCheckoutService } from '@bigcommerce/checkout-sdk';
 
 export default class CheckoutComponent extends React.PureComponent {
+    constructor(props) {
+        super(props);
+
+        this.service = createCheckoutService({
+            config: {
+                bigpayBaseUrl: 'https://bigpay.integration.zone',
+                storeHash: '7xcbg28tsc',
+                storeId: 12077240,
+                storeName: 'robin.ong+1521680812 testsworthy',
+            },
+        });
+    }
+
+    componentDidMount() {
+        this.service.loadCheckout()
+            .then(({ checkout }) => console.log(checkout.getCart()));
+    }
+
     render() {
         return (
             <section>
